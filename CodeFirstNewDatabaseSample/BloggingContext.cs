6a756e8ec99ec163f1014941e8580a34ab33ebd6
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-13 20:28:58 +0000

    Added CodeFirstNewDatabaseSameple project for EF Code First

---
+++ CodeFirstNewDatabaseSample/BloggingContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
