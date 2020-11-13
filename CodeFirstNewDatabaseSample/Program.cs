756fcfc7dd05989070f83c696a0a498d8a79d524
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-13 22:37:02 +0000

    Working so far

--- CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -1,8 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+// Microsoft Entity Framework
+//		https://docs.microsoft.com/en-us/ef/
+
+//		Entity Framework 6
+//			https://docs.microsoft.com/en-us/ef/ef6/
+//			Get started with Entity Framework 6
+//
+//          Tutorial
+//              https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/workflows/new-database
+//
+//              Database is created in SQLExpress when app is run first time
+//
+//              VS2019/Server Explorer
+//                                SERVER = laptop-qr0qpgvq\localdb#ec7f3d87.master.dbo (enterd as (LocalDB)\MSSQLLocalDB
+//                      CONNECTIONSTRING = Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=True
+//                  SSMS
+//                                SERVER = LAPTOP - QR0QPGVQ\LOCALDB#631D7789
+//                                    DB = CodeFirstNewDatabaseSample.BloggingContext
+
+//              MSSQL
+//                                DB = LAPTOP-QR0QPGVQ
+//                  CONNECTIONSTRING = Data Source=LAPTOP-QR0QPGVQ;Integrated Security=True
+//                  
 
 namespace CodeFirstNewDatabaseSample
 {
