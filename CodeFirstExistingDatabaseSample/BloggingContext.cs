6b07b09e0612a28f5576648c44b3b29a37299e58
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-14 12:53:31 +0000

    Tutorial complete and working

---
+++ CodeFirstExistingDatabaseSample/BloggingContext.cs
@@ -0,0 +1,19 @@
+using System.Data.Entity;
+
+namespace CodeFirstExistingDatabaseSample
+{
+    public partial class BloggingContext : DbContext
+    {
+        public BloggingContext()
+            : base("name=BloggingContext")
+        {
+        }
+
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
