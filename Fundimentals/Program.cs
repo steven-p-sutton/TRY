33b7bcabb45b6dff6e432bc0e15802bfd559387e
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-15 09:58:44 +0000

    added School database project to hold info and try out. Not using it for EF database tasks

---
+++ Fundimentals/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Microsoft Entity Framework
+//		https://docs.microsoft.com/en-us/ef/
+
+//		Entity Framework 6
+//			https://docs.microsoft.com/en-us/ef/ef6/
+
+//			Get started with Entity Framework 6
+//				https://docs.microsoft.com/en-us/ef/ef6/get-started
+//			Fundimentals
+//				Get Entity Framework
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/install
+//				Working with DbContext
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/working-with-dbcontext
+//				Relationships, navigation properties, and foreign keys
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/relationships
+//				Async query and save
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/async
+//				Configuration
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/configuring/code-based
+//				Connection management
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/connection-management
+//				Connection resiliency and retry logic
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/connection-resiliency/retry-logic
+//				Databinding with WinForms
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/databinding/winforms
+//				Working with disconnected entities
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/disconnected-entities/
+//				Logging and intercepting database operations
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/logging-and-interception
+//				Performance considerations for EF 4, 5, and 6
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/performance/perf-whitepaper
+//				Entity Framework 6 Providers
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/providers/
+//				Working with proxies
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/proxies
+//				Testing with a mocking framework
+//					https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
+//				Creating a Model
+//					https://docs.microsoft.com/en-us/ef/ef6/modeling/
+//				Querying and Finding Entities
+//					https://docs.microsoft.com/en-us/ef/ef6/querying/
+//				Saving Data with Entity Framework 6
+//					https://docs.microsoft.com/en-us/ef/ef6/saving/
+//
+//				School Sample Database (used in various samples)
+//					https://docs.microsoft.com/en-us/ef/ef6/resources/school-database
+//              Project Types
+//                  https://docs.microsoft.com/en-us/visualstudio/extensibility/internals/project-types?view=vs-2019
+//              Create SQL Server Database Project With Visual Studio
+//                  https://www.c-sharpcorner.com/article/create-sql-server-database-project-with-visual-studio/
+//                  
+//Code First Data Annotations
+//				https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations
+//		Entity Framework Core
+//			https://docs.microsoft.com/en-us/ef/core/
+
+//		References
+//			'Code First' to a New Database
+//			https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/workflows/new-database
+//			MS SQL Express (LocalDb)
+//			https://www.sqlshack.com/how-to-connect-and-use-microsoft-sql-server-express-localdb/
+
+
+namespace Fundimentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
