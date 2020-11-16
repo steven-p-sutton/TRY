e8bb32bbe0d0c1eb3ab64552e591ea57078ba25e
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-16 12:33:38 +0000

    Added User class & annotation for [Key]s

--- ConsoleApp1/CodeFirstNewDatabaseAnnotations/Post.cs
+++ ConsoleApp1/CodeFirstNewDatabaseAnnotations/Post.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConsoleApp1.CodeFirstNewDatabaseAnnotations
 {
     public class Post
     {
+        [Key]
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
