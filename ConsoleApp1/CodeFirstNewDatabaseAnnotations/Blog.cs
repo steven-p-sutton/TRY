74ec3155213cb707d89535b6a29e49e4c4af6cd4
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-16 12:33:38 +0000

    Added User class & annotation for [Key]s

--- ConsoleApp1/CodeFirstNewDatabaseAnnotations/Blog.cs
+++ ConsoleApp1/CodeFirstNewDatabaseAnnotations/Blog.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConsoleApp1.CodeFirstNewDatabaseAnnotations
 {
     public class Blog
     {
+        [Key]
         public int BlogId { get; set; }
         public string Name { get; set; }
+        public string Url { get; set; }
 
         public virtual List<Post> Posts { get; set; }
     }
--- ConsoleApp1/CodeFirstNewDatabaseAnnotations/Model1.cs
+++ ConsoleApp1/CodeFirstNewDatabaseAnnotations/Model1.cs
@@ -17,6 +17,7 @@ namespace ConsoleApp1.CodeFirstNewDatabaseAnnotations
         }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
 
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
