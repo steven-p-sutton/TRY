0dd9a53055733bf4df2c004e73c66f65abf4f512
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-18 09:59:38 +0000

    EFCoreGetStarted running ok

--- EFCoreGetStarted/Model.cs
+++ EFCoreGetStarted/Model.cs
@@ -9,8 +9,20 @@ namespace EFCoreGetStarted
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
+        // By default the db will be assumed to be the runtime folder and since the migration 
+        // has created it in the source code folder. On running 1st time there isn't a database 
+        // so a new blank one is created and therefore no tables are visvibe and an exception is 
+        // raised. Make sure you set the full path either a relateive 
+        //string myDataSource = "C:\\Users\\steve\\Documents\\Visual Studio Work\\TRY\\EFCoreGetStarted"
+        //    +"\\"+"blogging.db";
+        // 
+        // OR
+        //
+        // Copy yhr migrated db from the source folder to the deployment folder before running
+        string myDataSource = "blogging.db";
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=blogging.db");
+            => options.UseSqlite("Data Source=blogging.db"); 
     }
 
     public class Blog
