e954c58adffe5669fd79da9448124f444fb4eb02
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-18 09:59:38 +0000

    EFCoreGetStarted running ok

--- EFCoreGetStarted/Program.cs
+++ EFCoreGetStarted/Program.cs
@@ -1,6 +1,28 @@
 ﻿using System;
 using System.Linq;
 
+// 
+// Migrations Overview - Migrstion using cmd line
+//   https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
+//
+//   1. Build the model in code
+//   2. Create the db using migration
+//   3. Revise the model in code
+//   4. Upgrade the db using migration
+//   5. Repeat code -> upgrade steps as required
+//   6. Copy db from soiurec folder to deplyment folder before running, else exceptions will occur 
+//      (cant fins db so new blank created and tables are seen as missing, exception rised by app)
+// SQLite Viewer Download - use to view the blogging.db created by the migration
+//   https://www.systoolsgroup.com/sqlite-viewer.html
+//
+// References
+//
+//   SqliteDbContextOptionsBuilderExtensions.UseSqlite Method
+//      https://docs.microsoft.com/en-us/dotnet/api/microsoft.entityframeworkcore.sqlitedbcontextoptionsbuilderextensions.usesqlite?view=efcore-5.0
+//   C# (CSharp) DbContextOptionsBuilder.UseSqlite Examples
+//      https://csharp.hotexamples.com/examples/-/DbContextOptionsBuilder/UseSqlite/php-dbcontextoptionsbuilder-usesqlite-method-examples.html
+
+
 namespace EFCoreGetStarted
 {
     class Program
