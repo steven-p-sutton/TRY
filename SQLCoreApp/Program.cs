d1c72a65ef9129d85c22e10f0c32da3fef97ba68
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-20 18:17:18 +0000

    Added SQLCoreApp & SQLNetApp

---
+++ SQLCoreApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//
+// Try connection to a database
+//
+// https://www.c-sharpcorner.com/blogs/access-sql-server-database-in-net-core-console-application
+// 
+// NUGet 
+//      Microsoft.Extensions.Configuration
+//      Microsoft.Extensions.Configuration.FileExtensions
+//      Microsoft.Extensions.Configuration.Json
+//      System.Data.SqlClient
+//
+//
+
+namespace SQLCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
