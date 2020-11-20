6858db418461a9b527116858b5b4e7fe3b9863cd
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-20 22:16:07 +0000

    Country lapp from tutorial working in Widget MSSQL database LAPTOP-QR0QPGVQ

--- SQLCoreApp/Program.cs
+++ SQLCoreApp/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Data.SqlClient;
-//
+﻿//
 // Try connection to a database
 //
 // https://www.c-sharpcorner.com/blogs/access-sql-server-database-in-net-core-console-application
@@ -47,23 +45,72 @@ conn.ConnectionString =
 conn.Open();
 */
 
-// https://docs.microsoft.com/en-us/previous-versions/aspnet/jj653752(v=vs.110)?redirectedfrom=MSDN#sqlserver
+//  ASP.NET Data Access Options
+//      https://docs.microsoft.com/en-us/previous-versions/aspnet/ms178359(v=vs.110)
+//  SQL Server Connection Strings for ASP.NET Web Applications
+//      https://docs.microsoft.com/en-us/previous-versions/aspnet/jj653752(v=vs.110)?redirectedfrom=MSDN#sqlserver
+//  SQL Server 2012 Express LocalDB
+//      https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2012/hh510202(v=sql.110)?redirectedfrom=MSDN
+//  SQL Server Connection Strings for ASP.NET Web Applications
+//      https://docs.microsoft.com/en-us/previous-versions/aspnet/jj653752(v=vs.110)
+//  Connection strings and models (EntityFramework)
+//      https://docs.microsoft.com/en-us/ef/ef6/fundamentals/configuring/connection-strings?redirectedfrom=MSDN
 
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+//using ConsoleApp.Model;
+using Microsoft.Extensions.Configuration;
+using SQLCoreApp.DAL;
 
 namespace SQLCoreApp
 {
-    class Program
+   /*
+    * class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            SqlConnection conn = new SqlConnection();
-            conn.ConnectionString =
-              "Data Source=LAPTOP-QR0QPGVQ;" +
-              "Initial Catalog=Widget;" +
-              "Integrated Security=SSPI;";
-            conn.Open();
+            //SqlConnection conn = new SqlConnection();
+            //conn.ConnectionString =
+            //  "Data Source=LAPTOP-QR0QPGVQ;" +
+            //  "Initial Catalog=Widget;" +
+            //  "Integrated Security=SSPI;";
+            //conn.Open();
+            //
+        }
+    }
+}
+*/
+    class Program
+    {
+        private static IConfiguration _iconfiguration;
+        static void Main(string[] args)
+        {
+            GetAppSettingsFile();
+            PrintCountries();
+        }
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                                 .SetBasePath(Directory.GetCurrentDirectory())
+                                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }
+        static void PrintCountries()
+        {
+            var countryDAL = new CountryDAL(_iconfiguration);
+            var listCountryModel = countryDAL.GetList();
+            listCountryModel.ForEach(item =>
+            {
+                Console.WriteLine(item.Country);
+            });
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
         }
     }
 }
