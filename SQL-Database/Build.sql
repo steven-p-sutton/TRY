1c5c70ef1dd6a37f0781a23afc1d61a983353d9f
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-24 15:07:13 +0000

    Added some new projwct types & tried out Shared Code project type usimng .NET  & Core apps

---
+++ SQL-Database/Build.sql
@@ -0,0 +1,8 @@
+﻿--	SQL Server Data Tools for Visual Studio
+--	https://visualstudio.microsoft.com/vs/features/ssdt/
+--	Create SQL Server Database Project With Visual Studio
+--	https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2010/ff678491(v=vs.100)?redirectedfrom=MSDN
+--	Creating a Visual Studio Database Project for an existing SQL Server Database
+--	https://www.mssqltips.com/sqlservertip/2971/creating-a-visual-studio-database-project-for-an-existing-sql-server-database/
+--	Create a database and add tables in Visual Studio
+--	https://docs.microsoft.com/en-us/visualstudio/data-tools/create-a-sql-database-by-using-a-designer?view=vs-2019
