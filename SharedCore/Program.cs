285d0c4a04a4cf068dbed2c4d07d15d6f801923d
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-24 15:07:13 +0000

    Added some new projwct types & tried out Shared Code project type usimng .NET  & Core apps

---
+++ SharedCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// using WidgetObject from SharedCode project
+
+namespace SharedCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Net Widget World!");
+            WidgetObject w2 = new WidgetObject();
+            Console.WriteLine(w2.Display("Widget 2"));
+        }
+    }
+}
