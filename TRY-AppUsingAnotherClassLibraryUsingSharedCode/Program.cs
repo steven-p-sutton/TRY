3fb72d1d11746184d8446caab19b5b776b745103
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-25 16:25:38 +0000

    tidyupCoded up Shared Code project sharing - Running

---
+++ TRY-AppUsingAnotherClassLibraryUsingSharedCode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using nsAnotherClassLibraryUsingSharedCode;
+
+namespace nsAppUsingAnotherClassLibraryUsingSharedCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            csAnotherClassLibraryUsingSharedCode a = new csAnotherClassLibraryUsingSharedCode();
+            a.sAnotherClassLibraryUsingSharedCode = "Hello sAnotherClassLibraryUsingSharedCodeWorld!";
+            //csSharedCode.
+            Console.WriteLine(a.sAnotherClassLibraryUsingSharedCode);
+        }
+    }
+}
