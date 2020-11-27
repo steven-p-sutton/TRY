be3e8c26e76b50c9d11d5e6eef6e61edfd3a136a
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-27 12:43:41 +0000

    From Tutorial

---
+++ SQL-Database/dbo/Data/CountryX.sql
@@ -0,0 +1,16 @@
+﻿USE [SQL-Database];  
+GO  
+
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'The United States', 1)
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'Germany', 1)  ;
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'England', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'Netherlands', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'Italy', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'Brazil', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'South Africa', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'Japan', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'South Korea', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'North Korea', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'India', 1)  
+INSERT [dbo].[Country] ([country], [active]) VALUES (N'Russia', 1)  
+GO 
