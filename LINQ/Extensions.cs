65f7dd151379920407623c438ce4be565bc4a595
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-11 14:50:56 +0000

    update

---
+++ LINQ/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+    }
+}
