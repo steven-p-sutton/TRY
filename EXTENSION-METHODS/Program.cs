194ba56c94665f5102c6917056acae523bfedd29
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-11 15:12:44 +0000

    Added EXTENSION-METHOD project

---
+++ EXTENSION-METHODS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExtensionMethods;
+
+namespace EXTENSION_METHODS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            bool result = i.IsGreaterThan(100);
+
+            Console.WriteLine(result);
+        }
+    }
+}
