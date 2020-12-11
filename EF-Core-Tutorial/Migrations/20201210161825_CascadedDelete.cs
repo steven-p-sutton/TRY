d473db7a2b26b85165b1d0d4c2600bb74fc81e58
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-11 09:22:28 +0000

    WORKING: Added cascaded deletes on DB

---
+++ EF-Core-Tutorial/Migrations/20201210161825_CascadedDelete.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EF_Core_Tutorial.Migrations
+{
+    public partial class CascadedDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
