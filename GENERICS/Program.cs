f8e7818cc3c2dce2b18069230b0d7102c63016af
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-12 16:34:34 +0000

    Fixup & added GENERICS project

---
+++ GENERICS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GENERICS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GENERICS World!");
+        }
+    }
+}
---
+++ TEMPLATES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEMPLATES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello TEMPLATES World!");
+        }
+    }
+}
