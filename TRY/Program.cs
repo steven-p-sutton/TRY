4711f5781bad622cccc00519cd2c06ba2a319562
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-12 16:21:23 +0000

    Cleaned up TRY app

--- TRY/Program.cs
+++ TRY/Program.cs
@@ -10,37 +10,8 @@ namespace TRY
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TRY");
-            A A0 = new A();
-            Console.WriteLine(A0.a);
-
-            A A1 = new A("1st");
-            Console.WriteLine(A1.a);
-
-            A A2 = new A("First", "Second");
-            Console.WriteLine(A2.a);
-
-            A A3 = new A
-            {
-                a = "1"
-            };
-            Console.WriteLine(A3.a);
+            Console.WriteLine("Hello TRY World!");
         }
-    }
-    public class A
-    {
-        public A()
-        {
-        }
-        public A(string one)
-        {
-            a = one;
-        }
-        public A(string one, string two)
-        {
-            a = one + " " + two;
-        }
-        public string a;
     }
 }
   
