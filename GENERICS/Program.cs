6ff1288e13c6cfec96ea645398386a506d171bd4
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-12 20:02:21 +0000

    WORKING: Build & Test GenericList class

--- GENERICS/Program.cs
+++ GENERICS/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+//Generics<T>
+//    https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/
+//	  https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/generic-classes
+//    https://www.tutorialspoint.com/csharp/csharp_generics.htm
+//    https://www.tutorialsteacher.com/csharp/csharp-generics
+
 namespace GENERICS
 {
     class Program
@@ -7,6 +13,37 @@ namespace GENERICS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GENERICS World!");
+
+            Console.WriteLine("\n<int> is the type argument");
+
+            GenericList<int> list1 = new GenericList<int>();
+
+            for (int x = 0; x < 10; x++)
+            {
+                list1.AddHead(x);
+            }
+
+            foreach (int i in list1)
+            {
+                Console.Write(i + " \n");
+            }
+
+            Console.WriteLine("\n<string> is the type argument");
+            // 
+
+            GenericList<string> list2 = new GenericList<string>();
+
+            list2.AddHead("Steve");
+            list2.AddHead("Philip");
+            list2.AddHead("Sutton");
+
+            foreach (string s in list2)
+            {
+                Console.Write(s + " \n");
+            }
+
+            Console.WriteLine("\nDone");
+            Console.ReadLine();
         }
     }
 }
