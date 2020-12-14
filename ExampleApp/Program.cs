d10cff8366b8d89f241009de3ebc1216abc9bbc9
steven-p-sutton <steven.p.sutton@gmail.com>
2020-12-14 11:28:10 +0000

    Looking at Title for Example class and Go

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example1 steve = new Example1()
+            {
+               // Title = "Steve Example",
+               // Output = Console.Out
+            };
+            steve.Go("Hello Steve Example World!");
+        }
+    }
+}
