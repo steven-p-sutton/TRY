db37ef0c8ef190cd8822985ff559c84326405719
steven-p-sutton <steven.p.sutton@gmail.com>
2021-02-06 19:04:45 +0000

    updated comments

--- MOQ-Simple/Program.cs
+++ MOQ-Simple/Program.cs
@@ -2,6 +2,9 @@
 using Moq;
 
 // https://www.codeproject.com/articles/991028/using-moq-for-unit-testing
+//
+// Overview: Try an Employee, as yes not fully implemented, using a mocked, test only version
+// 
 
 namespace MOQ_Simple
 {
@@ -9,10 +12,30 @@ namespace MOQ_Simple
     {
         static void Main(string[] args)
         {
+            // Create a fake (mocked) version of our object using it's definition, either temp, or real, 
+            // but we do not want to use the unerlying implemetation and the mock wrappwe around it
+            // will prevent the real semantics being invoked. Instead the mock version will intercept and
+            // the redfined, mocked, semantics invokded instead.
+
+            // Crated a mocked wrapper containg the object we want (Employee). In reality it hives the object's
+            // interface and makes this available to define the mock semantics in a later step.
+
             var employee = new Mock<Employee>();
-            employee.Setup(x => x.GetDateofJoining(It.IsAny<int>())).Returns((int x) => DateTime.Now);
+
+            // Define the alternative, mocked, semantics for our object (Employee)
+            // takes an int & returns a DateTime (see Employee class below)
+
+            employee.Setup(x => x.GetDateofJoining
+            (
+                It.IsAny<int>())
+            )
+            .Returns((int x) => DateTime.Now);
+
+            // Define the test. In this case it uses the mocked object (Employee). It is important to understand that
+            // the mocked object is not the test subject, but instead the environment required.
 
             Console.WriteLine(employee.Object.GetDateofJoining(1));
+
             Console.ReadLine();
         }
     }
