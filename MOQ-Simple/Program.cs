ffb6e4d90c063cba8ac63570677f7dd7b9d4ef4b
steven-p-sutton <steven.p.sutton@gmail.com>
2021-02-06 18:35:07 +0000

    Added MOC Sample app

---
+++ MOQ-Simple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Moq;
+
+// https://www.codeproject.com/articles/991028/using-moq-for-unit-testing
+
+namespace MOQ_Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Mock<Employee>();
+            employee.Setup(x => x.GetDateofJoining(It.IsAny<int>())).Returns((int x) => DateTime.Now);
+
+            Console.WriteLine(employee.Object.GetDateofJoining(1));
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        public virtual DateTime GetDateofJoining(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
