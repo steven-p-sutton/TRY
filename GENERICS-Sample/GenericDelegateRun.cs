70d6ca29568bf942e7b1df39c933fe69ec162b7b
steven-p-sutton <steven.p.sutton@gmail.com>
2021-03-06 15:34:41 +0000

    update

--- GENERICS-Sample/GenericDelegateRun.cs
+++ GENERICS-Sample/GenericDelegateRun.cs
@@ -12,18 +12,18 @@ namespace nsGenericDelegate
             Console.WriteLine("Generic Delegate");
             Console.WriteLine("**************************************************************************");
 
-            GenericDelegate gd = new GenericDelegate();
+            GenericDelegate genericDelegate = new GenericDelegate();
 
             //create delegate instances
-            NumberChanger<int> nc1 = new NumberChanger<int>(gd.AddNum);
-            NumberChanger<int> nc2 = new NumberChanger<int>(gd.MultNum);
+            NumberChanger<int> nc1 = new NumberChanger<int>(genericDelegate.AddNum);
+            NumberChanger<int> nc2 = new NumberChanger<int>(genericDelegate.MultNum);
 
             //calling the methods using the delegate objects
             nc1(25);
-            Console.WriteLine("Value of Num: {0}", gd.getNum());
+            Console.WriteLine("Value of Num: {0}", genericDelegate.getNum());
 
             nc2(5);
-            Console.WriteLine("Value of Num: {0}", gd.getNum());
+            Console.WriteLine("Value of Num: {0}", genericDelegate.getNum());
             Console.WriteLine("\n");
 
         }
--- GENERICS-Sample/GenericMethod.cs
+++ GENERICS-Sample/GenericMethod.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace nsGenericMethod
+﻿namespace nsGenericMethod
 {
     class GenericMethod
     {
