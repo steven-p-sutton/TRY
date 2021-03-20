a193f3a597b3e36489f3cced8dce47345d8e81ad
steven-p-sutton <steven.p.sutton@gmail.com>
2021-03-20 18:26:53 +0000

    added XUnit Test project

---
+++ TEST-Employee/UnitTest.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using Mock;
+using nsEmployee;
+
+namespace TEST_Employee
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var employee = new MEmployee
+            {
+                Run = IMock.RunType.SUCCESS,
+                Arrange = IMock.RunType.SUCCESS,
+                Test = IMock.RunType.SUCCESS,
+                Assert = IMock.RunType.SUCCESS
+            };
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var employee = new MEmployee
+            {
+                Run = IMock.RunType.EXCEPTION,
+                Exception = new Exception("Employee Error"),
+                Throws = IMock.RunType.EXCEPTION,
+                Arrange = IMock.RunType.EXCEPTION,
+                Test = IMock.RunType.EXCEPTION,
+                Assert = IMock.RunType.EXCEPTION
+            };
+        }
+    }
+}
