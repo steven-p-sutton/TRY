96b3ad2032ae74231d081ae3e3c80c05bede527e
steven-p-sutton <steven.p.sutton@gmail.com>
2021-03-22 14:35:41 +0000

    updates

--- MOCK-Framework/IMock.cs
+++ MOCK-Framework/IMock.cs
@@ -1,23 +1,36 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MOCK.Framework
 {
     public class IMock
     {
         public enum RunType { SUCCESS = 0, EXCEPTION = 1 };
-        public Exception ExceptionExpected { set; get; }
-        public Exception ExceptionRaised { set; get; }
+        [JsonProperty("run")] 
         public RunType Run { set; get; }
-        public virtual bool Verifyable { set; get; }
-        public virtual bool Returns { set; get; }
-        public virtual bool ReturnsAsync { set; get; }
-        public virtual int Verify { set; get; }
-        public virtual RunType Throws { set; get; }
-        //public string Message { set; get; }
+        [JsonProperty("input")]
         public virtual bool Input { set; get; }
+        [JsonProperty("output")]
         public virtual bool Output { set; get; }
+        [JsonProperty("exceptionExpected")]
+        public Exception ExceptionExpected { set; get; }
+        [JsonProperty("exceptionRaised")]
+        public Exception ExceptionRaised { set; get; }
+        [JsonProperty("verifyable")]
+        public virtual bool Verifyable { set; get; }
+        [JsonProperty("returns")]
+        public virtual bool Returns { set; get; }
+        [JsonProperty("returnsAsync")]
+        public virtual bool ReturnsAsync { set; get; }
+        [JsonProperty("verify")]
+        public virtual int Verify { set; get; }
+        [JsonProperty("throws")]
+        public virtual RunType Throws { set; get; }
+        [JsonProperty("arrange")]
         public virtual RunType Arrange { set; get; }
+        [JsonProperty("test")]
         public virtual RunType Test { set; get; }
+        [JsonProperty("assert")]
         public virtual RunType Assert { set; get; }
     }
 }
