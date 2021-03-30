83838851c3553fd548c104253eadedc4f37bd1f1
steven-p-sutton <steven.p.sutton@gmail.com>
2021-03-30 10:18:12 +0100

    update

--- MODEL-Company/Company.cs
+++ MODEL-Company/Company.cs
@@ -6,8 +6,8 @@ namespace nsCompany
 {
     public class Company : ICompany
     {
-        public string Name() { throw new NotImplementedException(); }
-        public List<Employee> Employees() { throw new NotImplementedException(); }
-        public List<Widget> Widgets() { throw new NotImplementedException(); }
+        public string Name() { throw new NotImplementedException("Name"); }
+        public List<Employee> Employees() { throw new NotImplementedException("Employees"); }
+        public List<Widget> Widgets() { throw new NotImplementedException("Widgets"); }
     }
 }
